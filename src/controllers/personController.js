ac043812130a609a45582ee56cc913c16ee80f3d
Gabriel Araújo <54244466+gabrielaraujog9@users.noreply.github.com>
2022-10-18 03:09:10 +0000

    Initial commit

---
+++ src/controllers/personController.js
@@ -0,0 +1,23 @@
+var data = [];
+
+exports.get = (req, res, next) => {
+  res.status(201).send(data);
+};
+
+exports.post = (req, res, next) => {
+
+
+  
+  res.status(201).send(res.body)
+};
+
+exports.put = (req, res, next) => {
+  let id = req.params.id;
+  res.status(201).send(`Requisição PUT recebida com sucesso! ${id}`);
+};
+
+exports.delete = (req, res, next) => {
+  let id = req.params.id;
+  res.status(200).send(`Requisição DELETE recebida com sucesso! ${id}`);
+};
+
