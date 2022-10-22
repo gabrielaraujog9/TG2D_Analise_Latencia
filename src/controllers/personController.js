48f093fe03a461ae25a3694826d0cf8e9aa4d652
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2022-10-21 22:11:04 -0300

    Controllers exports

--- src/controllers/personController.js
+++ src/controllers/personController.js
@@ -1,26 +1,21 @@
 var data = ["Nome"];
-
-exports.get = (req, res, next) => {
-
-
-  
+var controller = {};
+controller.get = (req, res, next) => {
   res.status(201).send(data);
 };
-
-exports.post = (req, res, next) => {
-
-
-  
+controller.post = (req, res, next) => {
   res.status(201).send(res.body)
 };
 
-exports.put = (req, res, next) => {
+controller.put = (req, res, next) => {
   let id = req.params.id;
   res.status(201).send(`Requisição PUT recebida com sucesso! ${id}`);
 };
 
-exports.delete = (req, res, next) => {
+controller.delete = (req, res, next) => {
   let id = req.params.id;
   res.status(200).send(`Requisição DELETE recebida com sucesso! ${id}`);
 };
+
+module.exports = controller;
 
